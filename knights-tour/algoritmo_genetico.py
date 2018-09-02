fc255cc43d2cd1cb6740689db90d8f42eea7433f
Willian da Rosa <will.rosa.gvt@gmail.com>
2018-09-02 17:59:33 -0300

    Desenvolvendo seleção e reprodução da população

--- knights-tour/algoritmo_genetico.py
+++ knights-tour/algoritmo_genetico.py
@@ -1,5 +1,8 @@
 import random
 import math
+import sys
+import time
+
 
 class AlgoritmoGenetico:
 
@@ -15,17 +18,85 @@ class AlgoritmoGenetico:
 
         return individuals
 
-    def fitness(self, individuo_matriz):
+    def pegar_posicao_matriz_pelo_indece(self, matriz, indice):
+        tamanho_matriz = len(matriz)
 
-        sequencia_certa = 0
+        pos = {}
+        pos['linha'] = indice // tamanho_matriz
+        pos['coluna'] = indice % tamanho_matriz
 
-        for i in individuo_matriz:
+        return pos
 
-            sequencia_certa = sequencia_certa + 1
+    def valida_passo(self, individuo_matriz: list, pos_vetor_anterior, pos_vetor_atual):
 
-        return sequencia_certa
+        p_a = self.pegar_posicao_matriz_pelo_indece(individuo_matriz, pos_vetor_anterior)
 
-    def vetor2matriz(self, vetor):
+        p_atual = self.pegar_posicao_matriz_pelo_indece(individuo_matriz, pos_vetor_atual)
+
+        if (p_a['coluna'] == p_atual['coluna'] -1 and p_a['linha'] == p_atual['linha'] + 2) or \
+            (p_a['coluna'] == p_atual['coluna'] - 2 and p_a['linha'] == p_atual['linha'] + 1) or \
+            (p_a['coluna'] == p_atual['coluna'] + 1 and p_a['linha'] == p_atual['linha'] + 2) or \
+            (p_a['coluna'] == p_atual['coluna'] + 2 and p_a['linha'] == p_atual['linha'] + 1) or \
+            (p_a['coluna'] == p_atual['coluna'] + 2 and p_a['linha'] == p_atual['linha'] - 1) or \
+            (p_a['coluna'] == p_atual['coluna'] + 1 and p_a['linha'] == p_atual['linha'] - 2) or \
+            (p_a['coluna'] == p_atual['coluna'] - 1 and p_a['linha'] == p_atual['linha'] - 2) or \
+            (p_a['coluna'] == p_atual['coluna'] - 2 and p_a['linha'] == p_atual['linha'] - 1) :
+
+            return True
+
+
+
+        return False
+
+
+    '''
+    Melhor performace é 631
+    
+    '''
+    def fitness(self, individuo_vetor:list):
+
+        individuo_matriz = self.vetor2matriz(individuo_vetor)
+
+        pontuacoes = [0]
+        sequencia = True
+
+        pos_vetor_anterior = individuo_vetor.index(1)
+
+        i = 2
+
+        #vai percorrer todos os itens do vetor (64 posicao)
+        while i <= len(individuo_vetor):
+
+            pos_vetor_atual = individuo_vetor.index(i)
+
+            i = i + 1
+
+            validade = self.valida_passo(individuo_matriz, pos_vetor_anterior, pos_vetor_atual)
+
+            pos_vetor_anterior = pos_vetor_atual
+
+            if validade and sequencia:
+                pontuacoes[len(pontuacoes)-1] = pontuacoes[len(pontuacoes)-1] + 1
+            elif validade:
+                sequencia = True
+                pontuacoes.append(1)
+            else:
+                sequencia = False
+
+
+        somatorio = 0;
+
+        for i, valor in enumerate(pontuacoes):
+            multiplicador = 10 - i
+
+            if multiplicador < 0:
+                multiplicador = 0
+
+            somatorio = somatorio + (valor * multiplicador + 1)
+
+        return somatorio
+
+    def vetor2matriz(self, vetor:list):
 
         raiz = int(math.sqrt(len(vetor)))
 
@@ -45,10 +116,119 @@ class AlgoritmoGenetico:
 
         return vetor
 
+    def vetor_para_objeto_populacao_variavel(self, vetor):
+
+        individuo = {}
+        individuo['vida'] = 0
+        individuo['vetor'] = vetor
+
+        return individuo
+
+    def atribuir_expectativa_vida(self, individuo):
+
+        somatorio = self.fitness(individuo['vetor'])
+
+        idade = math.ceil((somatorio*10) / 631)
+
+        individuo['vida'] = idade
+
+        return individuo
 
 
-    def tecnica_selecao_populacao_variavel(self, populacao_inicial, num_max_geracoes):
-        pass
+    def selecao_populacao_variavel(self, populacao:list):
+
+        for i in populacao:
+            if i['vida'] == 0:
+                populacao.remove(i)
+
+        return populacao
+
+    def permutacao(self, individual1: list, individual2: list, ponto_corte):
+
+        novo_individual = individual1[:ponto_corte]
+
+        for i in individual2:
+            if not novo_individual.count(i):
+                novo_individual.append(i)
+
+        return novo_individual
+
+    def recombinacao_populacao_variavel(self, populacao:list, percentual_recombinacao:int):
+
+        tamanho_populacao = len(populacao)
+
+        num_recombinacao = int((tamanho_populacao*percentual_recombinacao)/100)
+
+        posicoes_aleatorias = list(range(tamanho_populacao))
+        random.shuffle(posicoes_aleatorias)
+
+        novos_individuos = []
+
+        for i in range(int(num_recombinacao / 2)):
+            ponto_corte = random.randint(1, (tamanho_populacao-1))
+
+            individual1 = populacao[posicoes_aleatorias[i]]['vetor']
+            individual2 = populacao[posicoes_aleatorias[(num_recombinacao - 1) - i]]['vetor']
+
+            novo_individuo1 = self.vetor_para_objeto_populacao_variavel(self.permutacao(individual1, individual2, ponto_corte))
+            novo_individuo2 = self.vetor_para_objeto_populacao_variavel(self.permutacao(individual2, individual1, ponto_corte))
+
+            self.atribuir_expectativa_vida(novo_individuo1)
+            self.atribuir_expectativa_vida(novo_individuo2)
+
+
+            novos_individuos.append(novo_individuo1)
+            novos_individuos.append(novo_individuo2)
+
+        return novos_individuos
+
+
+    def tecnica_selecao_populacao_variavel(self, populacao_inicial:list, num_max_geracoes, percentual_recombinacao, percentual_mutacao):
+
+        tempo = 1
+
+        populacao = []
+
+        for i in populacao_inicial:
+            individuo = self.vetor_para_objeto_populacao_variavel(i)
+            self.atribuir_expectativa_vida(individuo)
+            populacao.append(individuo)
+
+        while tempo < num_max_geracoes:
+
+
+
+            #populacao = self.selecao_populacao_variavel(populacao)
+
+            #novos_individuos = self.recombinacao_populacao_variavel(populacao, percentual_recombinacao)
+
+            #populacao = populacao + novos_individuos
+
+            print('populacao', tempo, len(populacao))
+
+
+            #tempo de espera para trabalhar na proxima geração
+            time.sleep(2)
+
+
+
+
+
+
+
+            tempo = tempo + 1
+
+
+
+
+
+
+
+
+
+
+
+
 
         #avaliacao
         #selecao
--- knights-tour/main.py
+++ knights-tour/main.py
@@ -6,21 +6,17 @@ from algoritmo_genetico import AlgoritmoGenetico
 #argumentos
 tamanho_inicial_populacao = 20
 num_max_geracoes = 100
+percentual_recombinacao = 50
+percentual_mutacao = 5
 
 
 ag = AlgoritmoGenetico()
 
-#populacao_inicial = ag.gerar_populacao_inicial(ag.gerar_individuo_modelo(), tamanho_inicial_populacao)
 
-#ag.tecnica_selecao_populacao_variavel(populacao_inicial, num_max_geracoes)
-
+#exemplo_funcional = [63, 20, 3, 24, 59, 36, 5, 26, 2, 23, 64, 37, 4, 25, 58, 35, 19, 62, 21, 50, 55, 60, 27, 6, 22, 1, 54, 61, 38, 45, 34, 57, 53, 18, 49, 44, 51, 56, 7, 28, 12, 15, 52, 39, 46, 31, 42, 33, 17, 48, 13, 10, 43, 40, 29, 8, 14, 11, 16, 47, 30, 9, 32, 41]
 
 
-matriz = ag.vetor2matriz(ag.gerar_individuo_modelo())
+populacao_inicial = ag.gerar_populacao_inicial(ag.gerar_individuo_modelo(), tamanho_inicial_populacao)
 
-
-s = ag.fitness(matriz)
-
-print(s)
-
+ag.tecnica_selecao_populacao_variavel(populacao_inicial, num_max_geracoes, percentual_recombinacao, percentual_mutacao)
 
